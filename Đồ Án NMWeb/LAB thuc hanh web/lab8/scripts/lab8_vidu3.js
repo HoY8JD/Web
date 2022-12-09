aad2264560c9914a093708e6607fd21cd3e2ce0e
VoNhaatHaoo2008 <vonhathao20082002@gmail.com>
2022-12-09 18:34:24 +0700

    re

--- Đồ Án NMWeb/LAB thuc hanh web/lab8/scripts/lab8_vidu3.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-function run()
-{
-	var s= new String(document.title);
-	s=s.substr(1) + s.charAt(0);
-	document.title=s;
-	window.status=s;
-	setTimeout('run()',500);
-}
--- Đồ Án NMWeb/LAB thuc hanh web/lab8/scripts/lab8_vidu4.js
+++
@@ -1,5 +0,0 @@
-// JavaScript Document
-function MauNen()
-{
-	document.bgColor=document.frm.mau.options[document.frm.mau.selectedIndex].value;
-}
